e713491b7bd1bdd34d1686e03a15832bc6583846
Austin Crane <austincrane@gmail.com>
2018-09-10 15:31:57 -0400

    top page message

--- inc/top-page-message.php
+++ inc/top-page-message.php
@@ -1,1 +1,3 @@
-Have questions? Need assistance? Please contact us at <a href="mailto:info@leanagiletraining.com">info@leanagiletraining.com</a>
+<div class="top-page-message">
+	Have questions? Need assistance? Please contact us at <a href="mailto:info@leanagiletraining.com">info@leanagiletraining.com</a>
+</div>
